ede282bb7274783ff1da8f5cdb1ca333370e9a48
Stephen Sorriaux <stephen.sorriaux@gmail.com>
2023-03-10 18:03:34 -0500

    Add testing to GA

--- test/verifyImageLabels.sh
+++ test/verifyImageLabels.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -e
 
-VCS_REF=$(docker inspect -f '{{ index .Config.Labels "org.label-schema.vcs-ref"}}' wurstmeister/kafka)
+VCS_REF=$(docker inspect -f '{{ index .Config.Labels "org.label-schema.vcs-ref"}}' ssorriaux/kafka:"${DOCKER_IMAGE_VERSION}")
 echo "VCS_REF=$VCS_REF"
 if [ -z "$VCS_REF" ] || [ "$VCS_REF" = "unspecified" ]; then
   echo "org.label-schema.vcs-ref is empty or unspecified"
   exit 1
 fi
-if ! git cat-file -e "$VCS_REF^{commit}"; then
-  echo "$VCS_REF Not a valid git commit"
-  exit 1
-fi
+#
+# if ! git cat-file -e "$VCS_REF^{commit}"; then
+#   echo "$VCS_REF Not a valid git commit"
+#   exit 1
+# fi
 
-BUILD_DATE=$(docker inspect -f '{{ index .Config.Labels "org.label-schema.build-date"}}' wurstmeister/kafka)
+BUILD_DATE=$(docker inspect -f '{{ index .Config.Labels "org.label-schema.build-date"}}' ssorriaux/kafka:"${DOCKER_IMAGE_VERSION}")
 echo "BUILD_DATE=$BUILD_DATE"
 if ! date -d "$BUILD_DATE"; then
   echo "$BUILD_DATE Not a valid date"
